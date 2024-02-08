0a647437a69ba2406029c4b53eb1612b75f7d1f4
Sanzid <sanzid94@gmail.com>
2024-02-08 22:42:25 +0600

    Cross on any image

---
+++ diagonal_cross_any_image.py
@@ -0,0 +1,21 @@
+import numpy as np
+from PIL import Image
+
+def main():
+    with Image.open('input_images/img6.jpg') as imp_img:
+        gray_image = imp_img.convert("L")
+        gray_image.show()
+        gray_image_array = np.array(gray_image)
+        ratio = len(gray_image_array)/len(gray_image_array[0])
+
+        for i in range(len(gray_image_array)):
+            for j in range(len(gray_image_array[0])):
+                if (i> ratio*j-25 and i< ratio*j+25) or ((i > len(gray_image_array) -25-ratio*j) and (i < len(gray_image_array) +25-ratio*j)):
+                    gray_image_array[i][j]=0
+        gray_image = Image.fromarray(gray_image_array)
+        gray_image.show()
+        gray_image.save('output_images/cross_any.jpg')
+
+
+if __name__ == '__main__':
+    main()
